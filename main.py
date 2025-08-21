8547aaca32f08c3d4ee823501cc3ebf34d36def9
Никита Остапенко <ostapenko.nikits@yandex.ru>
2025-08-21 15:14:34 +1000

    fixes

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import asyncio
 import os
 from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher, Router
+from aiogram.client.bot import DefaultBotProperties
 from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
 from aiogram import F
 import commands
@@ -32,44 +33,40 @@ start_kb = ReplyKeyboardMarkup(
     resize_keyboard=True
 )
 
-
 # Обработчики команд и кнопок
 @router.message(F.text == "/start")
 async def start_cmd(message: Message):
     await message.answer("Алло бедолага, выбери действие:", reply_markup=main_kb)
-
 
 @router.message(F.text == "Увеличить пенис")
 async def dick_cmd(message: Message):
     response = commands.increase_dick(message.from_user.id, message.from_user.username)
     await message.answer(response)
 
-
 @router.message(F.text == "Оскорбиться")
 async def offend_cmd(message: Message):
     response = commands.get_offended()
     await message.answer(response)
-
 
 @router.message(F.text == "Мой размер")
 async def my_size_cmd(message: Message):
     response = commands.get_current_size(message.from_user.id)
     await message.answer(response)
 
-
 @router.message(F.text == "Таблица пенисов")
 async def leaderboard_cmd(message: Message):
     response = commands.get_leaderboard()
     await message.answer(response, parse_mode="Markdown")
 
-
 # Функция запуска бота
 async def main():
-    bot = Bot(token=os.getenv("BOT_TOKEN"), parse_mode="HTML")
+    bot = Bot(
+        token=os.getenv("BOT_TOKEN"),
+        default=DefaultBotProperties(parse_mode="HTML")
+    )
     dp = Dispatcher()
     dp.include_router(router)
     print("Бот запущен и работает через polling!")
-    # Запуск long polling без портов
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
